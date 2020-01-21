4d4b5d279d7a216cd3930421880f3ce89d1d7ad7
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-01-21 19:10:19 +0300

    Added "param point_x and y" to "Navigation" in def

--- path_2_top_finder.py
+++ path_2_top_finder.py
@@ -9,8 +9,8 @@ general_field = [
     [0, 0, 0, 0, 0]
 ]
 
-start_point_y = 1
-start_point_x = 1
+start_point_y = 2
+start_point_x = 2
 start_point = general_field[start_point_y][start_point_x]
 
 
@@ -19,18 +19,53 @@ class MyClass:
 
 
 def path_2_top_finder(field, point_x, point_y, admin=0):
-    # Ориентация в пространстве
-    center = field[point_y][point_x]
-    top_left = field[point_y - 1][point_x - 1]
-    top_top = field[point_y - 1][point_x]
-    top_right = field[point_y - 1][point_x + 1]
-    right = field[point_y][point_x + 1]
-    bottom_right = field[point_y + 1][point_x + 1]
-    bottom_bottom = field[point_y + 1][point_x]
-    bottom_left = field[point_y + 1][point_x - 1]
-    left = field[point_y][point_x - 1]
+    """
+    Навигация: (значение в точке; точка Х; точка У)
 
-    # Режим отладки
+    :param field: general лист
+    :param point_x: "координата" по вложенным листам второго уровня ([][]) внутри general листа
+    :param point_y: "координата" по вложенным листам первого уровня([]) внутри general листа
+    :param admin: Включение режима отладки
+    :return: 
+    """
+    center = (
+        field[point_y][point_x],
+        point_y,
+        point_x)
+    top_left = (
+        field[point_y - 1][point_x - 1],
+        point_y - 1,
+        point_x - 1)
+    top_top = (
+        field[point_y - 1][point_x],
+        point_y - 1,
+        point_x)
+    top_right = (
+        field[point_y - 1][point_x + 1],
+        point_y - 1,
+        point_x + 1)
+    right = (
+        field[point_y][point_x + 1],
+        point_y,
+        point_x + 1)
+    bottom_right = (
+        field[point_y + 1][point_x + 1],
+        point_y + 1,
+        point_x + 1)
+    bottom_bottom = (
+        field[point_y + 1][point_x],
+        point_y + 1,
+        point_x)
+    bottom_left = (
+        field[point_y + 1][point_x - 1],
+        point_y + 1,
+        point_x - 1)
+    left = (
+        field[point_y][point_x - 1],
+        point_y,
+        point_x - 1)
+
+    # Вывод при включении режима отладки (admin=1)
     if admin:
         print(f"{center=}")
         print(f"{top_left=}")
@@ -44,4 +79,3 @@ def path_2_top_finder(field, point_x, point_y, admin=0):
 
 
 path_2_top_finder(general_field, start_point_x, start_point_y, 1)
-
