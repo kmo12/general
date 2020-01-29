be7d9261cab8c83f8d3340241bb0099bf94b382e
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-01-29 18:51:04 +0300

    Testing classes features

--- testClass.py
+++ testClass.py
@@ -1,16 +1,24 @@
 class Robot:
     def __init__(self, alive=1):
-        self.alive = alive
+        self.__alive = alive
         self.health = 100
         self.motivation = 100
 
+    def __repr__(self):
+        return "Этот робот не живой, просто симуляция для потакания потребностям начинающего прогера"
+
+    # Этот "трюк" помогает сделать атрибут alive читаемым извне, но не изменяемым
+    def get_alive(self):
+        return self.__alive
+    alive = property(get_alive)
+
     def checking_condition(self, current_health):
         if current_health <= 0:
-            self.alive = 0
+            self.__alive = 0
             print("\nAaand  it's dead, you are a monster...")
 
     def hurting(self, power=0):
-        if self.alive == 1:
+        if self.__alive == 1:
             if power:
                 if 1 < power < 5:
                     self.health -= 10
@@ -27,11 +35,15 @@ class Robot:
             print("No, you can't hurt a robot, it's already dead.")
 
 
+Robot = Robot()
 
-Robot().hurting(3)
-print("")
-Robot().hurting(5)
-print("")
-Robot().hurting(7)
-print("")
-Robot().hurting(10)
+
+print(Robot.alive)
+
+# Robot().hurting(3)
+# print("")
+# Robot().hurting(5)
+# print("")
+# Robot().hurting(7)
+# print("")
+# Robot().hurting(10)
