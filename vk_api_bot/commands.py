d8a80a69922228eed47437140180f7609ff111d7
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-01-29 19:00:15 +0300

    Testing VK API LongPoll
    Main file to start in main.py.
    It works with VK group chat (https://vk.com/public188524220). After start the bot just text "Анекдот" to try it.

---
+++ vk_api_bot/commands.py
@@ -0,0 +1,27 @@
+from random import randint
+
+import vk_api.vk_api
+from config import group_token
+
+import feedparser
+
+
+vkApi_token = vk_api.VkApi(token=group_token())
+vkApi = vkApi_token.get_api()
+
+
+def send_message(send_id, message):
+    vkApi.messages.send(peer_id=send_id, message=message, random_id=randint(1, 999999999))
+
+
+def random_joke(random_item=0):
+    """
+    :param random_item: int 0-9, or it will be randint(0-9)
+    :return: random 'fresh joke of the day' (one of 10 in collection)
+    """
+    random_item = randint(0, 9)
+    return feedparser.parse("https://www.anekdot.ru/rss/export_j.xml")["entries"][random_item]['summary']
+
+
+if __name__ == "__main__":
+    print(random_joke())
---
+++ vk_api_bot/main.py
@@ -0,0 +1,46 @@
+from commands import *
+
+import time
+
+# import vk_api.vk_api
+
+from vk_api.bot_longpoll import VkBotLongPoll
+from vk_api.bot_longpoll import VkBotEventType
+
+
+LPS_params = {"vk": vkApi_token,
+              "group_id": 188524220,
+              "v": 5.103}
+
+long_poll = VkBotLongPoll(LPS_params["vk"], group_id=LPS_params["group_id"], wait=25)
+
+
+def received_message(message=""):
+    """
+    :param message: str
+    :return: True if received from user message is equal with param 'message'
+    or return just received from user message
+    """
+
+    if message:
+        if event.object.message["text"] == message:
+            return True
+        else:
+            return False
+
+    return event.object.message["text"]
+
+
+# Перезагрузить скрипт и сообщение отправится в конфу Quadro
+# send_message(47289987, "Сообщение")
+
+#  "Listening" for actions
+for event in long_poll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        if received_message():
+            print(event, "\n", f"{event.object.message['from_id']}:", event.object.message["text"])
+
+        if received_message("Анекдот") or received_message("анекдот"):
+            send_message(event.object.message["peer_id"], random_joke())
+            print("Анекдот показан")
+            continue
