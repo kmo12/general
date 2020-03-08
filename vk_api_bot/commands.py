2f62dbff7530f512f01a2dd8a7399a557168b70c
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-03-08 20:33:47 +0300

    Reworked defs, give_list def added

--- vk_api_bot/commands.py
+++ vk_api_bot/commands.py
@@ -14,16 +14,35 @@ def send_message(send_id, message):
     vkApi.messages.send(peer_id=send_id, message=message, random_id=randint(1, 999999999))
 
 
-def random_joke(random_item=0):
-    """
-    :param random_item: int 0-9, or it will be randint(0-9)
-    :return: random 'fresh joke of the day' (one of 10 in collection)
-    """
-    random_item = randint(0, 9)
-    return feedparser.parse("https://www.anekdot.ru/rss/export_j.xml")["entries"][random_item]['summary']
+class TenJokes:
+    def give_joke(self, joke_num=False):
+        """
+        Отдаёт одну из 10 "шуток дня по версии anekdot.ru".
+        Если указать int (0-9), то отдаст шутку по номеру.
+        Без параметра: отдаст случайную.
+
+        Warning: чаще всего шутки крайней степени унылости.
+
+        :param joke_num: int. 0-9, or it will be randint(0-9)
+        :return: str: random 'fresh joke of the day' (one of 10 in collection)
+        """
+        if not joke_num:
+            random_joke_num = randint(0, 9)
+            return feedparser.parse("https://www.anekdot.ru/rss/export_j.xml")["entries"][random_joke_num]['summary']
+        else:
+            return feedparser.parse("https://www.anekdot.ru/rss/export_j.xml")["entries"][joke_num]['summary']
+
     # TODO: Добавить, чтобы вначале добавлял все анекдоты в список и при показе удалял отдельные.
     #  когда все анекдоты закончатся, спросить "по второму кругу?" (решение в добавлении копии списка и в возврате к ниму)
+    def give_list(self):
+        """
+        Отдаёт все 10 "шуток дня по версии anekdot.ru" списком.
+        :return: list
+        """
+        return [self.give_joke(i) for i in range(10)]
 
 
 if __name__ == "__main__":
-    print(random_joke())
+    # print(TenJokes().give_list())
+    # give_a_joke()
+    # print(give_a_joke())
