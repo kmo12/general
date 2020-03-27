dd78376bc98212bd24d522e49ec0fc2e9706d986
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-03-27 19:06:26 +0300

    Good second O(n) version of func have added

--- anagrams_and_testing.py
+++ anagrams_and_testing.py
@@ -1,24 +1,69 @@
-def anagram(s1: str, s2: str) -> bool:  # O(n)
-    a = "".join(s1.split())
-    b = "".join(s2.split())
-    if len(a) == len(b):
-        for i in range(len(a)):
-            if a[i] not in b:
-                return False
+# O(n)
+def string_to_array(string: str) -> list:
+    array = list()
+    for letter in string:
+        array.append(letter)
+    return array
+
+
+# O(n**2)
+def anagram_1(s1: str, s2: str) -> bool:
+    if len(s1) != len(s2):
+        return False
+    a = string_to_array(s1)
+    b = string_to_array(s2)
+    a.sort()  # O(n**2)
+    b.sort()  # O(n**2)
+
+    print(f"\n{a=}\n{b=}")
+
+    if a == b:
+        return True
+    else:
+        return False
+
+    # Жалко удалять придуманное решение, работающее на sorted array
+    # for index, letter in enumerate(a):
+    #     if a[index] == b[index]:
+    #         continue
+    #     else:
+    #         result = False
+    # return result
+
+
+# O(n)
+def anagram_2(s1: str, s2: str) -> bool:
+    if len(s1) != len(s2):
+        return False
+    array1 = [0 for x in range(26)]
+    array2 = [0 for x in range(26)]
+    a_pos = 97
+
+    for char in s1:
+        pos = ord(char) - a_pos
+        array1[pos] += 1
+
+    for char in s2:
+        pos = ord(char) - a_pos
+        array2[pos] += 1
+
+    if array1 == array2:
         return True
     else:
         return False
 
 
-def anagram_testing():  # O(1)
+# O(1)
+def anagram_testing(anagram_analizator):
     testing_dict = {"abc": "cba",  # True
                     "trie": "rtie",  # True
                     "qwe": "qwr",  # False
-                    "test": " test1",  # False
-                    "welcome home": "home weleomc"}  # True
+                    "tessting": "testting",  # False
+                    "welcomehome": "homeweleomc"}  # True
 
     for a, b in testing_dict.items():
-        print(f"{a} and {b}: {anagram(a, b)}")
+        print(f"{a} and {b}: {anagram_analizator(a, b)}")
 
 
-anagram_testing()
+if __name__ == "__main__":
+    anagram_testing(anagram_2)
