1785191a85775bed8a2d132263598da861a2c12b
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-04-01 00:29:07 +0300

    Changed vkApi names and start working with ApiResponse class

--- vk_api_bot/commands.py
+++ vk_api_bot/commands.py
@@ -1,17 +1,24 @@
 from random import randint
 
 import vk_api.vk_api
-from config import group_token
+from vk_api_bot.config import group_token
 
 import feedparser
 
 
-vkApi_token = vk_api.VkApi(token=group_token())
-vkApi = vkApi_token.get_api()
+vkApi = vk_api.VkApi(token=group_token())
+vkApi_get_api = vkApi.get_api()
 
 
 def send_message(send_id, message):
-    vkApi.messages.send(peer_id=send_id, message=message, random_id=randint(1, 999999999))
+    vkApi_get_api.messages.send(peer_id=send_id, message=message, random_id=randint(1, 999999999))
+
+
+class ApiResponse:
+    # TODO сюда можно добавлять методов для получения ответов, типа give_user_first_name и так далее
+    @staticmethod
+    def give_user_name(received_from_id: int) -> str:
+        return vkApi.method("users.get", {"user_ids": received_from_id})
 
 
 class TenJokes:
@@ -33,16 +40,15 @@ class TenJokes:
             return feedparser.parse("https://www.anekdot.ru/rss/export_j.xml")["entries"][joke_num]['summary']
 
     # TODO: Добавить, чтобы вначале добавлял все анекдоты в список и при показе удалял отдельные.
-    #  когда все анекдоты закончатся, спросить "по второму кругу?" (решение в добавлении копии списка и в возврате к ниму)
-    def give_list(self):
+    #  когда все анекдоты закончатся, спросить "по второму кругу?" (решение в добавлении копии списка и в возврате к нему)
+    @classmethod
+    def give_list(cls):
         """
         Отдаёт все 10 "шуток дня по версии anekdot.ru" списком.
         :return: list
         """
-        return [self.give_joke(i) for i in range(10)]
+        return [cls.give_joke(i) for i in range(10)]
 
 
 if __name__ == "__main__":
-    # print(TenJokes().give_list())
-    # give_a_joke()
-    # print(give_a_joke())
+    pass
