c7ac362d043ad41d6b1fa22c0908d8443c666b33
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-04-03 00:23:18 +0300

    class GetUserInfo, hello() have added

--- vk_api_bot/commands.py
+++ vk_api_bot/commands.py
@@ -1,10 +1,9 @@
-from random import randint
+from random import randint, choice
 
 import vk_api.vk_api
 from vk_api_bot.config import group_token
 
 import feedparser
-
 
 vkApi = vk_api.VkApi(token=group_token())
 vkApi_get_api = vkApi.get_api()
@@ -14,11 +13,45 @@ def send_message(send_id, message):
     vkApi_get_api.messages.send(peer_id=send_id, message=message, random_id=randint(1, 999999999))
 
 
-class ApiResponse:
-    # TODO сюда можно добавлять методов для получения ответов, типа give_user_first_name и так далее
-    @staticmethod
-    def give_user_name(received_from_id: int) -> str:
-        return vkApi.method("users.get", {"user_ids": received_from_id})
+class GetUserInfo:
+    def __init__(self, user_id: int):
+        self._user_id = user_id
+
+    case_dict = {"именительный": "nom",
+                 "родительный": "gen",
+                 "дательный": "dat",
+                 "винительный": "acc",
+                 "творительный": "ins",
+                 "предложный": "abl"}
+
+    def general_info(self, case="") -> dict:
+        if case:
+            return vkApi.method("users.get", {"user_ids": self._user_id,
+                                              "fields": "sex",
+                                              "name_case": self.case_dict[case]})[0]
+        # VKApi пришлёт именительный падеж, если не пришло условия для другого
+        return vkApi.method("users.get", {"user_ids": self._user_id, "fields": "sex"})[0]
+
+    def __repr__(self):
+        return str(self.general_info())
+
+    def name(self, case="") -> str:
+        return self.general_info(case)['first_name']
+
+    def last_name(self, case="") -> str:
+        return self.general_info(case)['last_name']
+
+    def fullname(self, case="") -> str:
+        return self.last_name(case) + self.name(case)
+
+    def sex(self) -> int:
+        """
+        Отдаёт:
+        1 — женский;
+        2 — мужской;
+        0 — пол не указан.
+        """
+        return self.general_info()['sex']
 
 
 class TenJokes:
@@ -50,5 +83,29 @@ class TenJokes:
         return [cls.give_joke(i) for i in range(10)]
 
 
+# TODO чтобы сделать "добрый ДЕНЬ/ВЕЧЕР/УТРО" можно написать такую функцию:
+# import time
+# mytime = time.localtime()
+# myhour = mytime.tm_hour
+# if myhour == 0:
+#     print('It is midnight') полночь
+# elif myhour < 12:
+#     print ('It is AM') утро
+# elif myhour == 12:
+#     print('It is noon') полдень
+# else:
+#     print ('It is PM') день
+
+def hello(dialog_id, name=""):
+    if name:
+        answer_variables = [f"Привет, {name}!",
+                            f"{name} здравствуй.",
+                            f"{name}, здравствуйте.",
+                            f"Здравствуйте, {name}."]
+    else:
+        answer_variables = ["Привет!"]
+    send_message(dialog_id, choice(answer_variables))
+
+    
 if __name__ == "__main__":
     pass
