f7521e6424ed149a55ab5f96eb9a55d414bdc3e5
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-04-28 11:57:13 +0300

    Docs added, trash deleted

--- queue_abstract_data_structure.py
+++ queue_abstract_data_structure.py
@@ -2,13 +2,6 @@ from random import randint, choice
 
 
 class Queue:
-    #   push(item) - enqueue
-    #   pop() - dequeue
-    #   peek() - front
-    #   is_empty()
-    #   size()
-    #   back()
-
     def __init__(self):
         self.__data = list()
 
@@ -38,20 +31,34 @@ class Queue:
 
     def remove(self, item):
         self.__data.remove(item)
-
+    
+    # Cheat for debugging
     def __str__(self):
         return f"{self.__data}"
 
 
-def name_picker(amount=5):
+def name_picker(amount=5) -> list:
+    """
+    Will return amount of random guy's names
+    """
     real_names = ["Tom", "Bruce", "Ivan", "Jeremy", "John",
                   "Mark", "Steve", "Sven", "Lesly", "Gordon",
                   "Paul", "Peter", "Stevie", "Brian", "Stan",
                   "Frank", "Antony", "Matt", "Robert", "Woody"]
-    return [real_names.pop(real_names.index(choice(real_names))) for x in range(1, amount + 1)]
+    return [real_names.pop(real_names.index(choice(real_names))) for x in range(amount)]
 
 
 def hot_potato(players_amount=5, end_num=3):
+    """
+    Script will show you, what position you need to take to not leave the game
+      with entered conditions.
+    :param players_amount: int. How many players in game
+    :param end_num: int. How many passes must be completed before guy
+                            who ended with ball will leave
+    :return: str. Info: Who was the last guy, which didn't leave,
+                        and what was his number in queue at the beginning
+                          of the game
+    """
     players_list = name_picker(players_amount)
     players = Queue()
 
@@ -63,7 +70,9 @@ def hot_potato(players_amount=5, end_num=3):
             first_guy = players.dequeue()
             next_guy = players.front()
             print(f"{first_guy} passed a potato to {next_guy}")
+            
             players.enqueue(first_guy)
+            
         print(f"{players.dequeue()} gone away")
 
     return f"-------------\n" \
@@ -71,8 +80,6 @@ def hot_potato(players_amount=5, end_num=3):
            f"Every {end_num} did gone away.\n\n" \
            f"Last guy is {players.front()},\n" \
            f"his position was {players_list.index(players.front()) + 1}"
-
-    # return print(players)
 
 
 print(hot_potato())
