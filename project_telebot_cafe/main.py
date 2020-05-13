8a03a4588fa843ace7a548a54362473d6823b912
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-05-13 23:53:21 +0300

    def log and simple text handler added

--- project_telebot_cafe/main.py
+++ project_telebot_cafe/main.py
@@ -8,10 +8,28 @@ import telebot
 bot = telebot.TeleBot(api_token())
 
 
+def log_message(message):
+    print(message)
+
+
 if __name__ == '__main__':
+    log_message("Бот запущен")
+
     @bot.message_handler(commands=['start'])
     def start_handler(message):
         bot.send_message(message.chat.id, 'Привет, мир!')
+        log_message("Бот запущен")
 
+
+    @bot.message_handler(content_types=['text'])
+    def text_handler(message):
+        text = message.text.lower()
+        chat_id = message.chat.id
+        if text == "отзывы":
+            bot.send_message(chat_id, f'{commands.get_yandex_company_reviews(13156405625)}')
+            log_message(f"Отзывы показаны в чате №{chat_id}")
+        else:
+            bot.send_message(chat_id, 'Простите, я ваc не понял :(')
+            log_message(f"Сообщение не распознано: {message.text}")
 
     bot.polling(none_stop=False, interval=0, timeout=20)
