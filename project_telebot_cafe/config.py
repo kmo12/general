67f1f4f546ddd4292a5a032c60d116b4456defd5
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-05-13 23:31:25 +0300

    Add files via upload

---
+++ project_telebot_cafe/config.py
@@ -0,0 +1,10 @@
+from telebot import apihelper
+
+
+def api_token():
+    return ""
+
+
+# pip install pysocks
+# apihelper.proxy = {'https': 'socks5://5.252.161.48:1080/'}
+# apihelper.proxy = {'https': 'socks5://138.68.165.154:1080/'}
