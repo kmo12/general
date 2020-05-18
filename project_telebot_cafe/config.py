7124e9c69b45fae0d01fa45c1ed029add9b401f9
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-05-18 14:36:40 +0300

    Info changed a little

--- project_telebot_cafe/config.py
+++ project_telebot_cafe/config.py
@@ -1,6 +1,3 @@
-from telebot import apihelper
-
-
 # Before work you need to contact @BotFather in telegram and create an API token.
 # Than put it below in the empty string.
 def api_token():
@@ -10,6 +7,7 @@ def api_token():
 # In case you need to use proxy for connection.
 # Still can't make it work and using dedicated server.
 
-# pip install pysocks
+# >> pip install pysocks
+# from telebot import apihelper
 # apihelper.proxy = {'https': 'socks5://5.252.161.48:1080/'}
 # apihelper.proxy = {'https': 'socks5://138.68.165.154:1080/'}
