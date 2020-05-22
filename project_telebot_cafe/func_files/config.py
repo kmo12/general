82421e0ebc2b9bc468ad298738a80d6669d5e529
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-05-22 18:26:43 +0300

    Create config.py

---
+++ project_telebot_cafe/func_files/config.py
@@ -0,0 +1,16 @@
+# Before work you need to contact @BotFather in telegram and create an API token.
+# Than put it below in the empty string.
+def api_token():
+    return "0000000000:AAAAAAAaAaaAAAAAAaA_aaaAaaAaAaaAaaa"
+
+
+def yandex_corp_id():
+    return 000000000000
+
+
+# In case you need to use proxy for connection.
+# Still can't make it work and using dedicated server.
+
+# >> pip install pysocks
+# from telebot import apihelper
+# apihelper.proxy = {'https': 'socks5://0.000.000.00:0000/'}
