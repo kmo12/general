ce565dc1bda9fb0d9a7c97d58b468eb49a200e01
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-06-20 16:40:48 +0300

    Changing resolution from 1920x1080 to 1600x900 and back
    This script is useful for my laptop, when I connecting my TV with HDMI and need to switch resolution from 1600x900 (res. for work) on 1920x1080 (high res. for better visual)

--- ScreenResolutionChanger/main.py
+++ ScreenResolutionChanger/main.py
@@ -1,11 +1,28 @@
 import ctypes
-import subprocess
+import win32api
 
-
+# Узнаём текущее разрешение монитора
 user32 = ctypes.windll.user32
+user32.SetProcessDPIAware()
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
-print(screensize)
+# Меняем разрешение
+if screensize[0] == 1920:
+    mode = win32api.EnumDisplaySettings()
+    mode.PelsWidth = 1600
+    mode.PelsHeight = 900
+    mode.BitsPerPel = 32
 
-subprocess.call(['xrandr', '-s', '%dx%d' % resolution])
+    win32api.ChangeDisplaySettings(mode, 0)
 
+elif screensize[0] == 1600:
+    mode = win32api.EnumDisplaySettings()
+    mode.PelsWidth = 1920
+    mode.PelsHeight = 1080
+    mode.BitsPerPel = 32
+
+    win32api.ChangeDisplaySettings(mode, 0)
+
+else:
+    print("Изначальное разрешение экрана не установлено на 1920х1080 или на 1600х900!")
+    input("")
