89eaa3269a23e84205814d2248cf5fc220e4ec42
kmo12 <59850546+kmo12@users.noreply.github.com>
2020-07-29 19:20:54 +0300

    imports fixed, execute_read_query() fixed
    Also tests added

--- project_telebot_cafe/func_files/pg_database_execution.py
+++ project_telebot_cafe/func_files/pg_database_execution.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import config
-import database_commands as db_command
+try:
+    import func_files.config as config
+    import func_files.database_commands as db_command
+except ModuleNotFoundError:
+    import config
+    import database_commands as db_command
 
 import psycopg2
 
 
-class PGDataBaseExecutor:
+class PGDatabaseMainExecutor:
     def __init__(self, database, user, password, host, port):
         self.database = database
         self.user = user
@@ -54,6 +58,7 @@ class PGDataBaseExecutor:
         else:
             return print("Execution is not possible! Bad connection")
 
+        connection.autocommit = True
         result_info = ()
         cursor = connection.cursor()
         try:
@@ -69,25 +74,10 @@ class PGDataBaseExecutor:
 
 if __name__ == '__main__':
     db_info = config.pg_db_connection_info()
-    db_executor = PGDataBaseExecutor(db_info[0],  # database
-                                     db_info[1],  # user
-                                     db_info[2],  # password
-                                     db_info[3],  # host
-                                     db_info[4],  # port
-                                     )
+    db_executor = PGDatabaseMainExecutor(*db_info)
 
-    print(db_command.test_create_user("Михаил", "89104534545"))
+    print(db_executor.execute_read_query(db_command.insert_customer('name', '3456346', 34534)))
 
-    print(db_command.test_create_reservation("12.27.2020",
-                                             "18:30",
-                                             "Михаил",
-                                             "89104534545",
-                                             1))
+    print(db_executor.execute_read_query(db_command.select_customer_id_chat_id_from_customers()))
 
-    db_executor.execute_query(db_command.test_create_user("Михаил", "89104534545"))
-
-    db_executor.execute_query(db_command.test_create_reservation("12.27.2020",
-                                                                 "18:30",
-                                                                 "Михаил",
-                                                                 "89104534545",
-                                                                 1))
+    # ("12.27.2020", "18:30", "name", "89104534545", CUSTOMER_ID)
